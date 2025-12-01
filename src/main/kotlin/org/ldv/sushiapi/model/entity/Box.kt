ee66ee61d7b1e3e3a5c432f75f8c3af191287786
Neky547 <nekenarab55@gmail.com>
2025-12-01 10:40:31 +0100

    first commit

---
+++ src/main/kotlin/org/ldv/sushiapi/model/entity/Box.kt
@@ -0,0 +1,34 @@
+package org.ldv.sushiapi.model.entity
+
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.ManyToMany
+import jakarta.persistence.OneToMany
+
+class Box (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var boxId: Long,
+
+    var nom: String,
+    var nbPieces: Int,
+    var prix: Double,
+    var nomImage: String,
+
+    //Relation many to many des boxes aux saveurs :
+    @ManyToMany
+    @JoinTable(
+        name = "saveurs_boxes",
+        joinColumns = [JoinColumn(name="fk_box_id")],
+        inverseJoinColumns = [JoinColumn(name = "fk_saveur_id")]
+    )
+    val saveurs: MutableSet<Saveur>,
+
+    //Relation one to many d'une box à ses aliments
+    @OneToMany(mappedBy = "box")
+    val alimentBoxes: MutableList<AlimentBox>
+){
+}
